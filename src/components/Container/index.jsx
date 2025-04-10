f79d838656528b4b37b49b751b916c7530b1372c
Hazzembadran <hazemaqbadran@gmail.com>
2025-04-10 12:42:52 +0200

    setup

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.css'
+
+const Container = () => {
+  return (
+    <>
+      <div className="overlay"></div>
+      <div className="bg-circles">
+        <div className="circle-1"></div>
+        <div className="circle-2"></div>
+        <div className="circle-3"></div>
+        <div className="circle-4"></div>
+      </div>
+      <div className="main hidden"></div>
+      <div className="main container">{children}</div>
+    </>
+  );
+};
+
+export default Container;
