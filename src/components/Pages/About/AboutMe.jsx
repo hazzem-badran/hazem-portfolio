a2858387129a4cfec69cca443cdb9553f3570e62
Hazzembadran <hazemaqbadran@gmail.com>
2025-07-05 15:33:21 +0300

    fix: Update AboutMe component to include Expo in experience description

--- src/components/Pages/About/AboutMe.jsx
+++ src/components/Pages/About/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
       name="aboutMeDescription"
     >
       <strong>🎯 A Product-minded Developer</strong> specializing in frontend development with <strong>React.js</strong> and <strong>React Native</strong>, passionate about building modern, responsive, and user-friendly applications across web and mobile platforms.<br /><br />
-      I have strong experience with <strong>React.js</strong>, <strong>Next.js</strong>, and <strong>React Native</strong>, always committed to writing clean code and designing elegant interfaces that deliver great user experiences.<br /><br />
+      I have strong experience with <strong>React.js</strong>, <strong>Next.js</strong>, and <strong>React Native</strong> and <strong>Expo</strong>, always committed to writing clean code and designing elegant interfaces that deliver great user experiences.<br /><br />
       Clean code, simple design, and software that truly adds value are at the core of my work. I’m always eager to learn, grow, and collaborate with teams to turn ideas into successful applications.
     </p>
   );
