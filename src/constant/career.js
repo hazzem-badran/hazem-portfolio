187b8935762de5313800bed690f8a16b5c8011d7
Hazzembadran <hazemaqbadran@gmail.com>
2025-09-11 18:07:05 +0300

    feat(career): simplify title for React Developer entry

--- src/constant/career.js
+++ src/constant/career.js
@@ -1,7 +1,7 @@
 export const CAREER = [
   {
     date: "08/2025 – Present",
-    title: "React Developer (Web & Native) - ASK Consulting",
+    title: "React Developer - ASK Consulting",
     description:
       "Developing scalable web and mobile applications using React.js, React Native, and Next.js. Collaborating with cross-functional teams and integrating modern tools like Docker to deliver high-performance, user-friendly products.",
     link: {
